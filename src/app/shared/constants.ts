64f9ea93e1e5022d52c1424191bc396aa0de82e5
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-30 14:10:34 +0000

    updates

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -4,3 +4,7 @@ export const SERVER_URL = 'http://127.0.0.1:2024';
 export const VERIFY_URL = 'verify-url';
 export const GET_INFO = 'get-info';
 export const DOWNLOAD = 'download';
+
+/* MESSAGES */
+export const SEARCH_INSTRUCTIONS = 'PASTE YT Video Link to Download';
+export const INVALID_URL = 'Video URL Is INVALID';
--- src/app/shared/handlers.ts
+++ src/app/shared/handlers.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { DOWNLOAD, SERVER_URL } from './constants';
 
-export const handleVideoDownload = (http: HttpClient, body: {}) => {
+export const handleVideoDownload = (
+  http: HttpClient,
+  body: { ytVideoUrl: string },
+  videoTitle: string,
+  extension: string
+) => {
   http
     .post(`${SERVER_URL}/${DOWNLOAD}`, body, {
       responseType: 'arraybuffer',
     })
     .subscribe((data: ArrayBuffer) => {
-      const blob = new Blob([data], { type: 'video/mp4' });
+      const blob = new Blob([data], { type: `video/${extension}` });
       const url = URL.createObjectURL(blob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = 'new.mp4';
+      link.download = `${videoTitle}.${extension}`;
       link.click();
     });
 };
