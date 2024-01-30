74ba6f522896b9a8b552372cc55a55813c719f74
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-30 14:12:21 +0000

    updating store when video url is verified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,19 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DownloadComponent } from './download/download.component';
-import { ErrorComponent } from './error/error.component';
 import { LoadingComponent } from './loading/loading.component';
-import { GET_INFO, SERVER_URL, VERIFY_URL } from './shared/constants';
+import {
+  GET_INFO,
+  INVALID_URL,
+  SEARCH_INSTRUCTIONS,
+  SERVER_URL,
+  VERIFY_URL,
+} from './shared/constants';
 import { handleVideoDownload } from './shared/handlers';
 import { VideoDetailsComponent } from './video-details/video-details.component';
+import { MessageComponent } from './message/message.component';
+import { Store } from '@ngrx/store';
+import { changeTitle, changeUrl } from './store/store.actions';
 
 @Component({
   selector: 'app-root',
@@ -19,40 +27,46 @@ import { VideoDetailsComponent } from './video-details/video-details.component';
     LoadingComponent,
     VideoDetailsComponent,
     DownloadComponent,
-    ErrorComponent,
+    MessageComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  videoSrc = '';
   showVideoDetails = false;
-  showDownload = false;
   showLoading = false;
-  showError = false;
+  showMessage = true;
+
+  videoSrc = '';
   videoInfo: any = {};
+  message = SEARCH_INSTRUCTIONS;
 
   http = inject(HttpClient);
+  store = inject(Store);
 
   onSrcInputChange() {
+    // do nothing if search input is empty
     if (!this.videoSrc) {
-      this.showError = false;
+      this.showMessage = false;
       return;
     }
 
     this.showVideoDetails = false;
-    this.showError = false;
+    this.showMessage = false;
     this.showLoading = true;
 
+    // first, check if the URL is valid. Proceed to get info if its is
     const body = { ytVideoUrl: this.videoSrc };
     this.http
       .post(`${SERVER_URL}/${VERIFY_URL}`, body)
       .subscribe((response: any) => {
+        // if URL is invalid...
         if (!response.isValid) {
-          this.showDownload = false;
           this.showLoading = false;
-          this.showError = true;
+          this.message = INVALID_URL;
+          this.showMessage = true;
         } else {
+          // get video info
           this.showLoading = true;
           this.http
             .post(`${SERVER_URL}/${GET_INFO}`, body)
@@ -62,16 +76,19 @@ export class AppComponent {
                 formats: info.formats,
               };
 
+              console.log('app, video src: ', this.videoSrc);
+
+              // update the store
+              this.store.dispatch(
+                changeTitle({ title: info.videoDetails.title })
+              );
+              this.store.dispatch(changeUrl({ url: this.videoSrc }));
+              // @ts-ignore
+
               this.showVideoDetails = true;
-              this.showDownload = false;
               this.showLoading = false;
             });
         }
       });
-  }
-
-  onDownload() {
-    const body = { ytVideoUrl: this.videoSrc };
-    handleVideoDownload(this.http, body);
   }
 }
