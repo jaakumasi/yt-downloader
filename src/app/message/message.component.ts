fc6ed68db3571372860c7d76d0f33cb9378eb6b6
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-30 14:11:00 +0000

    implemented messages

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message',
   standalone: true,
   imports: [],
   templateUrl: './message.component.html',
-  styleUrl: './message.component.scss'
+  styleUrl: './message.component.scss',
 })
 export class MessageComponent {
-
+  @Input() message!: string;
 }
