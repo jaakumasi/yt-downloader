6fe5d62b71ce6fa6c71ad29bcf8fd75b7c6eee8d
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-30 14:13:01 +0000

    implemented download for a specific video quality

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { handleVideoDownload } from '../shared/handlers';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-download',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './download.component.html',
-  styleUrl: './download.component.scss'
+  styleUrl: './download.component.scss',
 })
-export class DownloadComponent {
+export class DownloadComponent implements AfterViewInit {
+  http = inject(HttpClient);
+  store = inject(Store);
+  storeObs$ = this.store.select('initStateReducer');
 
+  /* data [video title, format (qualityLabel (eg: 720p), container (eg: mp4), url)] */
+  @Input() formatDetails!: any;
+
+  ngAfterViewInit(): void {
+    console.log(this.formatDetails);
+  }
+
+  onDownload() {
+    this.storeObs$.subscribe((state) => {
+      const { title, url } = state;
+      console.log('state url: ', url);
+      console.log('state title: ', title);
+
+      const body = { ytVideoUrl: url };
+      handleVideoDownload(this.http, body, title, this.formatDetails.container);
+    });
+  }
 }
--- src/app/video-details/video-details.component.ts
+++ src/app/video-details/video-details.component.ts
@@ -1,21 +1,26 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { DownloadComponent } from '../download/download.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-video-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DownloadComponent],
   templateUrl: './video-details.component.html',
   styleUrl: './video-details.component.scss',
 })
 export class VideoDetailsComponent implements AfterViewInit {
   @Input() videoInfo!: any;
+  videosWithAudio!: any[];
+  showDownloadOptions = false;
 
   ngAfterViewInit(): void {
     //@ts-ignore
     const formats = this.videoInfo.formats;
-    // console.log(formats);
+    console.log(this.videoInfo);
 
-    const videosWithAudio = formats
+    this.videosWithAudio = formats
       .map((format: any) => {
         // check out formats that are either mp4 or webm and that have audio 🎼 attached
         if (
@@ -25,8 +30,9 @@ export class VideoDetailsComponent implements AfterViewInit {
         )
           return format;
       })
-      // removed 'undefined' values
+      // remove 'undefined' values
       .filter((value: any) => value !== undefined);
-    console.log(videosWithAudio);
+
+    this.showDownloadOptions = true;
   }
 }
