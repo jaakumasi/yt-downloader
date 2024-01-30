e5ca2a85e759f6bfd34492d7150009df835be84b
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-30 14:10:06 +0000

    reducer name

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,17 +1,11 @@
 import { isDevMode } from '@angular/core';
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer,
-} from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { initialStateReducer } from '../store/store.reducers';
 
 export interface State {}
 
 export const reducers: ActionReducerMap<State> = {
-  initialStateReducer: initialStateReducer,
+  initStateReducer: initialStateReducer,
 };
 
 export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
--- src/app/store/store.reducers.ts
+++ src/app/store/store.reducers.ts
@@ -4,16 +4,12 @@ import { changeTitle, changeUrl } from './store.actions';
 
 export const initialStateReducer = createReducer(
   initialState,
-  on(changeTitle, (state, action) => {
-    return {
-      ...state,
-      title: action.title,
-    };
-  }),
-  on(changeUrl, (state, action) => {
-    return {
-      ...state,
-      url: action.url,
-    };
-  })
+  on(changeTitle, (state, action) => ({
+    ...state,
+    title: action.title,
+  })),
+  on(changeUrl, (state, action) => ({
+    ...state,
+    url: action.url,
+  }))
 );
