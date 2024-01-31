bb3d4f57410af36973be01344b100ba28c2e2454
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-31 14:49:26 +0000

    pwa setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(reducers, { metaReducers })]
+  providers: [provideRouter(routes), provideStore(reducers, { metaReducers }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
